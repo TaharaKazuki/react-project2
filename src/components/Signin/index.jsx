0f06c133a7620e82501f4fddacd452a581440664
田原 一樹 <ssp0727.lnc@gmail.com>
2021-10-11 20:26:32 +0900

    update

--- src/components/Signin/index.jsx
+++ src/components/Signin/index.jsx
@@ -15,7 +15,9 @@ const SignIn = () => {
       password: ''
     },
     validationSchema: Yup.object({
-      email: Yup.string().email('Invalid email address').required('The email is required'),
+      email: Yup.string()
+        .email('Invalid email address')
+        .required('The email is required'),
       password: Yup.string().required('The email is required')
     }),
     onSubmit: (value) => {
@@ -24,22 +26,21 @@ const SignIn = () => {
     }
   })
 
-
   return (
     <div className="container">
-      <div className="signin_wrapper" style={{margin: '100px'}}>
+      <div className="signin_wrapper" style={{ margin: '100px' }}>
         <form onSubmit={formik.handleSubmit}>
           <h2>Please login</h2>
-          <input 
+          <input
             name="email"
             placeholder="Email"
             onChange={formik.handleChange}
-            onBlur={formik.handleBlur} 
+            onBlur={formik.handleBlur}
             value={formik.values.email}
           />
-          { formik.touched.email && formik.errors.email ? <div className="error_label">
-            { formik.errors.email }
-          </div> : null }
+          {formik.touched.email && formik.errors.email ? (
+            <div className="error_label">{formik.errors.email}</div>
+          ) : null}
           <input
             name="password"
             type="password"
@@ -47,11 +48,14 @@ const SignIn = () => {
             onBlur={formik.handleBlur}
             value={formik.values.password}
           />
-          { formik.touched.password && formik.errors.password ? <div className="error_label">
-            { formik.errors.password }
-          </div> : null}
-          { loading ? <CircularProgress color="secondary" className="progress" /> : 
-          <button type="submit">Log in</button>}
+          {formik.touched.password && formik.errors.password ? (
+            <div className="error_label">{formik.errors.password}</div>
+          ) : null}
+          {loading ? (
+            <CircularProgress color="secondary" className="progress" />
+          ) : (
+            <button type="submit">Log in</button>
+          )}
         </form>
       </div>
     </div>
