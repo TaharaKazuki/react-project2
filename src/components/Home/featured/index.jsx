9fa656edc4bb0c7e3364b6fcf674a68c1cafe7b3
田原 一樹 <ssp0727.lnc@gmail.com>
2021-10-15 20:44:11 +0900

    update

--- src/components/Home/featured/index.jsx
+++ src/components/Home/featured/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-// import Test from './test'
 import Stripes from './Stripe'
+import Text from './Text'
 
 const Featured = () => {
   return (
     <div className="featured_wrapper">
       <Stripes />
+      <Text />
     </div>
   )
 }
