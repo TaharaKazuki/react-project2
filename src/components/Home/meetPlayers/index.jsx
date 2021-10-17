5441d0fe0994db7faa8a9f28076ab5701499f447
田原 一樹 <ssp0727.lnc@gmail.com>
2021-10-17 16:11:35 +0900

    update

--- src/components/Home/meetPlayers/index.jsx
+++ src/components/Home/meetPlayers/index.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Tag } from '../../Utils/tools'
 import { Fade } from 'react-awesome-reveal'
+import HomeCards from './card'
 
 const tagDefault = {
   bck: '#0e1731',
@@ -9,6 +10,7 @@ const tagDefault = {
 }
 
 const MeetPlayers = () => {
+  const [show, setShow] = useState(false)
   const showTextTag = (text) => (
     <Tag
       {...tagDefault}
@@ -22,11 +24,20 @@ const MeetPlayers = () => {
   )
 
   return (
-    <Fade triggerOnce>
+    <Fade
+      onVisibilityChange={(inView) => {
+        if (inView) {
+          setShow(true)
+        }
+      }}
+      triggerOnce
+    >
       <div className="home_meetplayers">
         <div className="container">
           <div className="home_meetplayers_wrapper">
-            <div className="home_card_wrapper">card</div>
+            <div className="home_card_wrapper">
+              <HomeCards show={show} />
+            </div>
             <div className="home_text_wrapper">
               <div>{showTextTag('Meet')}</div>
               <div>{showTextTag('The')}</div>
